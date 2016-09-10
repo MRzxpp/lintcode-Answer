f1c87f12f5be5aeed7d9bc34c0990adb6dc09b3a
shuzhi.zhong <szzhong@sandemarine.com>
2016-09-10 17:42:16 +0800

    MinimumSize

---
+++ src/MinimumSize.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Zhongsz on 2016/9/5.
+ */
+public class MinimumSize {
+    /**
+     * @param nums: an array of integers
+     * @param s:    an integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // write your code here
+
+        return 0;
+    }
+}
