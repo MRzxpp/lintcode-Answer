17e881902539661250e9ed174aaafa2eaaffea61
shuzhi.zhong <szzhong@sandemarine.com>
2016-09-12 17:54:33 +0800

    MinimumSize Completed + ThreadTest

--- src/MinimumSize.java
+++ src/MinimumSize.java
@@ -1,15 +1,91 @@
+import java.util.ArrayList;
+
+import static java.lang.Math.min;
+
 /**
  * Created by Zhongsz on 2016/9/5.
  */
 public class MinimumSize {
     /**
-     * @param nums: an array of integers
-     * @param s:    an integer
+     * @param nums : an array of integers
+     * @param s    :    an integer
      * @return: an integer representing the minimum size of subarray
+     * 时间复杂度为O（n2）
      */
     public int minimumSize(int[] nums, int s) {
         // write your code here
+//        nums = new int[]{2, 3, 1, 2, 4, 3};
+//        nums = new int[]{3, 5, 1, 2, 2, 2};
+        nums = new int[]{9, 1, 8, 2, 7, 3, 6, 4, 5, 10};
+        s = 55;
+        int sum, len = nums.length;
+        ArrayList<Integer> result = new ArrayList<>();
+        ArrayList<Integer> resultStore = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            result.clear();
+            sum = 0;
+            if (nums[i] >= s) {
+                resultStore.clear();
+                resultStore.add(nums[i]);
+                return resultStore.size();
+            }
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                result.add(nums[j]);
+                if (sum >= s && result.size() <= len) {
+                    len = result.size();
+                    resultStore.clear();
+                    resultStore.addAll(result);
+                    break;
+                }
 
-        return 0;
+            }
+        }
+        if (resultStore.size() == 0) {
+            return -1;
+        }
+
+        return resultStore.size();
+    }
+
+    /**
+     * 时间复杂度为O(nlog2n)
+     *
+     * @param nums
+     * @param s
+     * @return
+     */
+
+    public int minimumSizeIndicator(int[] nums, int s) {
+        int result = 2147483647;
+        int sum = 0;
+        int start = 0;
+        int end = 0;
+        for (; end < nums.length; end++) {
+            sum += nums[end];
+            if (sum >= s) {
+                result = min(result, end - start + 1);
+                for (; start <= end; start++) {
+                    if (sum >= s) {
+                        sum -= nums[start];
+                        result = min(result, end - start + 1);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        if (result == 2147483647) {
+            return -1;
+        }
+        return result;
+    }
+
+
+    public static void main(String args[]) {
+        int[] a = new int[]{2, 3, 1, 2, 3, 3};
+        int b = 7;
+        MinimumSize minimumSize = new MinimumSize();
+        System.out.println(minimumSize.minimumSizeIndicator(a, b));
     }
 }
---
+++ src/TestThread.java
@@ -0,0 +1,66 @@
+/**
+ * Created by Zhongsz on 2016/9/12.
+ */
+class NewThread implements Runnable {
+    Thread t;
+
+    NewThread() {
+        // 创建第二个新线程
+        t = new Thread(this, "Demo Thread");
+        System.out.println("Child thread: " + t);
+        t.start(); // 开始线程
+    }
+
+    // 第二个线程入口
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Child Thread: " + i);
+                // 暂停线程
+                Thread.sleep(0);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Exiting child thread.");
+    }
+}
+
+class NewThread2 implements Runnable {
+    Thread t;
+    NewThread2(){
+        t=new Thread(this,"ChildChild");
+        System.out.println("ChildChild: "+t);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("ChildChild Thread: " + i);
+                // 暂停线程
+                Thread.sleep(0);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Exiting childchild thread.");
+    }
+}
+
+public class TestThread {
+    public static void main(String args[]) {
+        new NewThread(); // 创建一个新线程
+        new NewThread2();
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Main Thread: " + i);
+                Thread.sleep(1);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+        System.out.println("Main thread exiting.");
+    }
+}
